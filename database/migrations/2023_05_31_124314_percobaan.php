b0faa95e6e8340a5b47aee24d4865fd471ba49fb
Endra <endra.kartika.25@gmail.com>
2023-05-31 19:43:41 +0700

    test

--- database/migrations/2023_05_31_124314_percobaan.php
+++ database/migrations/2023_05_31_124314_percobaan.php
@@ -13,12 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('articles', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->text('description');
-            $table->timestamps();
-        });
+        //
     }
 
     /**
@@ -28,6 +23,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('articles');
+        //
     }
 };
